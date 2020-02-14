8a36365f4e3b14eea1f97796a8793f4e70ad55fe
YuigaWada <yuigawada@gmail.com>
2020-02-14 21:16:46 +0900

    podをクリーンインストール

--- Pods/XLPagerTabStrip/Sources/PagerTabStripViewController.swift
+++ Pods/XLPagerTabStrip/Sources/PagerTabStripViewController.swift
@@ -109,6 +109,11 @@ open class PagerTabStripViewController: UIViewController, UIScrollViewDelegate {
         childController.view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         containerView.addSubview(childController.view)
         childController.didMove(toParent: self)
+        
+        viewControllers.forEach {
+            addChild($0)
+            containerView.addSubview($0.view)
+        }
     }
 
     open override func viewWillAppear(_ animated: Bool) {
