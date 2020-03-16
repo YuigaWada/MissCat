5284e707ddbe17a2fb6317778a5c9680590bd9b3
YuigaWada <yuigawada@gmail.com>
2020-03-16 13:19:23 +0900

    ThemeViewController: デザインを調整

--- MissCat/View/Settings/ThemeViewController.swift
+++ MissCat/View/Settings/ThemeViewController.swift
@@ -20,78 +20,48 @@ public class ThemeViewController: UITableViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         setTables()
+        setTableView()
     }
     
     private func setTables() {
-        // <テーマ>
-        // 一般
-        // ・メインカラー default: .systemBlue
-        // ・背景色 default: .white
-        // ・境界線 default:  gray
         //
         tables[.General] = [.init(title: "メインカラー", currentColor: .systemBlue),
                             .init(title: "背景色", currentColor: .white),
                             .init(title: "境界線", currentColor: .lightGray)]
         
-        // 投稿
-        //
-        // [投稿のMock]
-        //
-        // ・文字色 text
-        // ・リンク色 link
-        // ・リアクション
-        // ・自分のリアクション
-        // ・文字サイズ
-        //
         
         tables[.Post] = [.init(type: .Mock),
-                         .init(title: "文字色", currentColor: .systemBlue),
-                         .init(title: "リンク色", currentColor: .white),
+                         .init(title: "文字色", currentColor: .black),
+                         .init(title: "リンク色", currentColor: .systemBlue),
                          .init(title: "リアクションセル", currentColor: .lightGray),
-                         .init(title: "自分のリアクション", currentColor: .lightGray),
+                         .init(title: "自分のリアクション", currentColor: .systemOrange),
                          .init(title: "文字サイズ", type: .Size)]
         
-        // [引用RNのMock]
-        // [RNのMock]
-        //
-        // ・Renote文字 renote
-        // ・引用RNのボーダー comment-renote-border
-        //
-        //
-        
         tables[.Renote] = [.init(type: .Mock),
-                           .init(title: "Renoteしたユーザー名", currentColor: .systemBlue),
-                           .init(title: "引用RNのボーダー", currentColor: .white)]
-        
-        // [リプライのMock]
-        //
-        // ・ 背景 reply-background
-        // ・文字色 reply-text
-        //
+                           .init(title: "Renoteしたユーザー名", currentColor: .systemGreen),
+                           .init(title: "引用RNのボーダー", currentColor: .systemBlue)]
         
         tables[.Reply] = [.init(type: .Mock),
-                          .init(title: "背景", currentColor: .white),
-                          .init(title: "文字色", currentColor: .lightGray)]
+                          .init(title: "背景", currentColor: .lightGray),
+                          .init(title: "文字色", currentColor: .black)]
         
-        // 通知欄
-        //
-        // [通知のMock]
-        //
-        // ・リアクション
-        // ・RN
-        // ・文字色
         tables[.Notifications] = [.init(type: .Mock),
-                                  .init(title: "リアクション", currentColor: .systemBlue),
-                                  .init(title: "Renote", currentColor: .white),
-                                  .init(title: "文字色", currentColor: .lightGray)]
+                                  .init(title: "リアクション", currentColor: .systemRed),
+                                  .init(title: "Renote", currentColor: .systemGreen),
+                                  .init(title: "文字色", currentColor: .black)]
+    }
+    
+    private func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     private func setColorDisplay(on parent: UIView, currentColor: UIColor?) {
         let view = UIView()
-        let edge = self.view.frame.height * 0.7
+        let edge = parent.frame.height * 0.7
         
-        view.frame = CGRect(x: self.view.frame.origin.x - edge - 20,
-                            y: self.view.frame.center.y - edge / 2,
+        view.frame = CGRect(x: parent.frame.width - edge - 20,
+                            y: parent.frame.center.y - edge / 2,
                             width: edge,
                             height: edge)
         
@@ -99,26 +69,71 @@ public class ThemeViewController: UITableViewController {
         view.layer.borderWidth = 1
         view.backgroundColor = currentColor ?? .white
         
+        view.translatesAutoresizingMaskIntoConstraints = false
         parent.addSubview(view)
         view.layoutIfNeeded()
         view.layer.cornerRadius = edge / 2
+        
+        parent.addConstraints([
+            NSLayoutConstraint(item: view,
+                               attribute: .width,
+                               relatedBy: .equal,
+                               toItem: parent,
+                               attribute: .width,
+                               multiplier: 0,
+                               constant: edge),
+            
+            NSLayoutConstraint(item: view,
+                               attribute: .height,
+                               relatedBy: .equal,
+                               toItem: parent,
+                               attribute: .height,
+                               multiplier: 0,
+                               constant: edge),
+            
+            NSLayoutConstraint(item: view,
+                               attribute: .right,
+                               relatedBy: .equal,
+                               toItem: parent,
+                               attribute: .right,
+                               multiplier: 1.0,
+                               constant: -20),
+            
+            NSLayoutConstraint(item: view,
+                               attribute: .centerY,
+                               relatedBy: .equal,
+                               toItem: parent,
+                               attribute: .centerY,
+                               multiplier: 1.0,
+                               constant: 0)
+        ])
     }
     
     // MARK: TableViewDelegate
     
-    public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    public override func numberOfSections(in tableView: UITableView) -> Int {
         return tables.keys.count
     }
     
-    public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section]
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = Section(rawValue: section) else { return 0 }
+        return tables[section]?.count ?? 0
+    }
+    
+    public override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = .clear
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         
         guard let section = Section(rawValue: indexPath.section),
-            let tables = tables[section] else { return cell }
+            let tables = tables[section],
+            indexPath.row < tables.count else { return cell }
         
         let table = tables[indexPath.row]
         cell.textLabel?.text = table.title
