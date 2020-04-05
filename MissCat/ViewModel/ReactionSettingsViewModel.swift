1bd062af670b34f9ba3f8dc910c38ffbe297db09
YuigaWada <yuigawada@gmail.com>
2020-04-05 22:54:32 +0900

    リファクタ

--- MissCat/ViewModel/ReactionSettingsViewModel.swift
+++ MissCat/ViewModel/ReactionSettingsViewModel.swift
@@ -29,21 +29,38 @@ class ReactionSettingsViewModel: ViewModelType {
         self.disposeBag = disposeBag
     }
     
+    // MARK: Publics
+    
+    /// 編集中かどうかstateを変更
+    /// - Parameter editState: true: 変更中
+    func changeEditState(_ editState: Bool) {
+        state.editting = editState
+    }
+    
+    /// UserDefaultsからお気に入り絵文字を取得する
     func setEmojiModel() {
         emojis = EmojiModel.getEmojis(type: .favs) ?? []
         updateEmojis(emojis)
     }
     
+    /// 指でセルを移動させた後、実際に絵文字モデルも変更させる
+    /// - Parameters:
+    ///   - sourceIndexPath: sourceIndexPath
+    ///   - destinationIndexPath: destinationIndexPath
     func moveItem(moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let tempNumber = emojis.remove(at: sourceIndexPath.item)
         emojis.insert(tempNumber, at: destinationIndexPath.item)
         updateEmojis(emojis)
     }
     
+    /// 絵文字をcollectionViewの最末端へ追加する
+    /// - Parameter emojiModel: EmojiView.EmojiModel
     func addEmoji(_ emojiModel: EmojiView.EmojiModel) {
         emojis.append(emojiModel)
         updateEmojis(emojis)
     }
+    
+    // MARK: Utilities
     
     private func updateEmojis(_ items: [EmojiView.EmojiModel]) {
         let section = ReactionGenViewController.EmojisSection(items: items)
