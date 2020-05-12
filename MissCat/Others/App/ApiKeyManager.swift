24c02915e53aa9afd1380abe86c521232d5d06f4
YuigaWada <yuigawada@gmail.com>
2020-05-12 19:44:03 +0900

    通知に対応

---
+++ MissCat/Others/App/ApiKeyManager.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKeyManager.swift
+//  MissCat
+//
+//  Created by Yuiga Wada on 2020/05/12.
+//  Copyright © 2020 Yuiga Wada. All rights reserved.
+//
+
+import Foundation
+
+struct ApiKeyManager: ApiKeyManagerProtocol {
+    var baseUrl = ""
+}
--- MissCat/Others/App/AppDelegate.swift
+++ MissCat/Others/App/AppDelegate.swift
@@ -6,76 +6,69 @@
 //  Copyright © 2019 Yuiga Wada. All rights reserved.
 //
 
-import AWSCognito
-import AWSSNS
 import CoreData
+import FirebaseCore
+import FirebaseInstanceID
+import FirebaseMessaging
 import MisskeyKit
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     // MARK: Main
     
     var window: UIWindow?
+    private let gcmMessageIDKey = "gcm.message_id"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         setupMissCat()
-        // setupCognito()
+        setupFirebase()
         setupNotifications(with: application)
+        registerSw()
         
         return true
     }
     
-    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        // バックグラウンドで実行する処理
-//        notification()
-        completionHandler(.newData)
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
                      fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        let appState = application.applicationState
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
         
-//        switch appState {
-//        case .active:
-//            break
-//        case .inactive:
-//            notification()
-//        case .background:
-//            notification()
-//        @unknown default:
-//            notification()
-//        }
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        completionHandler(UIBackgroundFetchResult.newData)
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        // Device Tokenの取得
-        let deviceTokenString = deviceToken.map { String(format: "%.2hhx", $0) }.joined()
-        saveDeviceToken(deviceTokenString)
-        
-        let sns = AWSSNS.default()
-        guard let request = AWSSNSCreatePlatformEndpointInput() else { return }
-        
-        request.token = deviceTokenString
-        request.platformApplicationArn = "arn:aws:sns:ap-northeast-1:098581475404:app/APNS_SANDBOX/MissCat"
-        request.customUserData = "{\"lang\":\"ja\"}" // 言語情報を渡す
-        
-        sns.createPlatformEndpoint(request).continueWith(executor: AWSExecutor.mainThread(), block: { task in
-            guard task.error == nil,
-                let result = task.result,
-                let subscribeInput = AWSSNSSubscribeInput() else { return nil }
-            
-            subscribeInput.topicArn = "arn:aws:sns:ap-northeast-1:098581475404:app/APNS_SANDBOX/MissCat"
-            subscribeInput.endpoint = result.endpointArn
-            subscribeInput.protocols = "Application"
-            sns.subscribe(subscribeInput)
-            
-            self.saveEndpointArn(result.endpointArn)
-            return nil
-        })
-    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {}
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print(error)
@@ -84,28 +77,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     // MARK: Setup
     
     private func setupMissCat() {
-        if let currentInstance = Cache.UserDefaults.shared.getCurrentLoginedInstance(),
-            !currentInstance.isEmpty {
-            MisskeyKit.changeInstance(instance: currentInstance)
-            _ = EmojiHandler.handler
-        }
-        
         Theme.shared.set()
-//        if let currentUserId = Cache.UserDefaults.shared.getCurrentLoginedUserId() {
-//            Theme.shared.set(userId: currentUserId)
-//        }
     }
     
-    private func setupCognito() {
-        // Amazon Cognito
-        let credentialsProvider = AWSCognitoCredentialsProvider(regionType: .APNortheast1,
-                                                                identityPoolId: "ap-northeast-1:70a071ae-23f9-46e8-b0f1-d5c9000e4f29")
-        AWSServiceManager.default().defaultServiceConfiguration = AWSServiceConfiguration(region: .APNortheast1, credentialsProvider: credentialsProvider)
+    private func setupFirebase() {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
     }
     
     private func setupNotifications(with application: UIApplication) {
-        application.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
         checkNotifyPermission(with: application)
+        application.registerForRemoteNotifications()
+    }
+    
+    private func registerSw() {
+        let misscatApi = MisscatApi(apiKeyManager: MockApiKeyManager())
+        misscatApi.registerSw()
     }
     
     // MARK: Notifications
@@ -116,138 +103,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { granted, error in
             guard error == nil, granted else { return }
             print("通知許可")
-//            DispatchQueue.main.async {
-//                application.registerForRemoteNotifications()
-//            }
         })
-    }
-    
-    private func notification() {
-        // UserDefaultsに保存してから通知を流すように
-        guard let current = Cache.UserDefaults.shared.getLatestNotificationId(),
-            !current.isEmpty else { return }
-        
-        MisskeyKit.notifications.get(limit: 20, following: false) { results, error in
-            guard let results = results, results.count > 0, error == nil else { return }
-            
-            if let notificationId = results[0].id { // 最新の通知をsave
-                Cache.UserDefaults.shared.setLatestNotificationId(notificationId)
-            }
-            
-            for index in 0 ..< results.count {
-                let model = results[index]
-                
-                guard model.id != current else { break }
-                self.sendNotification(of: model) // 順に通知を出していく
-            }
-        }
-    }
-    
-    private func sendNotification(of model: NotificationModel) {
-        guard let type = model.type else { return }
-        
-        var contents: NotificationContents?
-        switch type {
-        case .follow:
-            contents = getFollowNotification(of: model)
-        case .mention:
-            contents = getReplyNotification(of: model)
-        case .reply:
-            contents = getReplyNotification(of: model)
-        case .renote:
-            contents = getRenoteNotification(of: model)
-        case .quote:
-            contents = getCommentRenoteNotification(of: model)
-        case .reaction:
-            contents = getReactionNotification(of: model)
-        case .pollVote:
-            break
-        case .receiveFollowRequest:
-            contents = getFollowNotification(of: model)
-        default:
-            return
-        }
-        
-        if let contents = contents {
-            sendNotification(title: contents.title, body: contents.body)
-        }
-    }
-    
-    private func getReplyNotification(of model: NotificationModel) -> NotificationContents? {
-        guard let user = model.user, let note = model.note else { return nil }
-        let name = getDisplayName(user)
-        let text = note.text ?? ""
-        
-        let hasFile = (note.files?.count ?? 0) > 0
-        let hasFileTitle = hasFile ? "画像を添付して" : ""
-        
-        return .init(title: "\(name)さんが\(hasFileTitle)返信しました",
-                     body: text)
-    }
-    
-    private func getFollowNotification(of model: NotificationModel) -> NotificationContents? {
-        guard let user = model.user else { return nil }
-        let name = getDisplayName(user)
-        
-        return .init(title: "",
-                     body: "\(name)さんがフォローしました")
-    }
-    
-    private func getRenoteNotification(of model: NotificationModel) -> NotificationContents? {
-        guard let user = model.user, let note = model.note else { return nil }
-        let name = getDisplayName(user)
-        let text = note.text ?? ""
-        
-        return .init(title: "\(name)さんがRenoteしました",
-                     body: text)
-    }
-    
-    private func getCommentRenoteNotification(of model: NotificationModel) -> NotificationContents? {
-        guard let user = model.user, let note = model.note else { return nil }
-        let name = getDisplayName(user)
-        let text = note.text ?? ""
-        
-        let hasFile = (note.files?.count ?? 0) > 0
-        let hasFileTitle = hasFile ? "画像を添付して" : ""
-        
-        return .init(title: "\(name)さんが\(hasFileTitle)引用Renoteしました",
-                     body: text)
-    }
-    
-    private func getReactionNotification(of model: NotificationModel) -> NotificationContents? {
-        guard let user = model.user, let note = model.note, let reaction = model.reaction else { return nil }
-        let name = getDisplayName(user)
-        let text = note.text ?? ""
-        
-        return .init(title: "\(name)さんがリアクション\"\(reaction)\"を送信しました",
-                     body: text)
-    }
-    
-    private func sendNotification(title: String, body: String) {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
-        content.sound = UNNotificationSound.default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(identifier: "misscat.\(UUID().uuidString)", content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request)
-    }
-    
-    private func getDisplayName(_ user: UserModel) -> String {
-        guard let name = user.name else { return user.username ?? "" }
-        return name.count > 13 ? String(name.prefix(13)) : name
-    }
-    
-    // MARK: UserDefaults
-    
-    private func saveDeviceToken(_ token: String) {
-        UserDefaults.standard.set(token, forKey: "deviceToken")
-    }
-    
-    private func saveEndpointArn(_ endpointArn: String?) {
-        guard let endpointArn = endpointArn else { return }
-        UserDefaults.standard.set(endpointArn, forKey: "endpointArn")
     }
     
     // MARK: UISceneSession Lifecycle
