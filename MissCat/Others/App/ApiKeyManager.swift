70bbf2cb196787481f17e1e640c6720a52bb2612
YuigaWada <yuigawada@gmail.com>
2023-02-22 18:27:30 +0900

    [add] ApiKeyManager.swift

---
+++ MissCat/Others/App/ApiKeyManager.swift
@@ -0,0 +1,9 @@
+//
+//  ApiKeyManager.swift
+//  MissCat
+//
+//  Created by Yuiga Wada on 2020/05/12.
+//  Copyright © 2020 Yuiga Wada. All rights reserved.
+//
+
+struct ApiKeyManager: ApiKeyManagerProtocol { var baseUrl = "" }
